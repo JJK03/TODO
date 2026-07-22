c5b1878811b76b1eede267306e711e09422705b9
JJK03 <wkdwlsrb03@gmail.com>
2026-07-22 09:18:15 +0900

    to env

--- frontend/src/api/todoAPI.ts
+++ frontend/src/api/todoAPI.ts
@@ -1,6 +1,6 @@
 ﻿import type { Todo } from "../types/todo";
 
-const API_BASE_URL = "http://localhost:9170";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getTodos = async (completedFilter = "all"): Promise<Todo[]> => {
   const url =
